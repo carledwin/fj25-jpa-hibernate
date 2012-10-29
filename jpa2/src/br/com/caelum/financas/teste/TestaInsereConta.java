9e7bf697ee593c18b58fa0ad1b422d3f993b83d5
Raphael A. Silva <rkhaotix@gmail.com>
2012-10-29 15:01:06 -0200

    Testes de CRUD

--- jpa2/src/br/com/caelum/financas/teste/TestaInsereConta.java
+++ jpa2/src/br/com/caelum/financas/teste/TestaInsereConta.java
@@ -1,28 +1,29 @@
 package br.com.caelum.financas.teste;
 
 import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.Persistence;
 
+import br.com.caelum.financas.dao.ContaDAO;
+import br.com.caelum.financas.infra.JPAUtil;
 import br.com.caelum.financas.modelo.Conta;
 
 public class TestaInsereConta {
 	public static void main(String[] args) {
 		long inicio = System.currentTimeMillis();
 		
-		EntityManagerFactory factory = Persistence.createEntityManagerFactory("controlefinancas");
-		EntityManager entityManager = factory.createEntityManager();
+		EntityManager entityManager = new JPAUtil().getEntityManager();
+		ContaDAO dao = new ContaDAO(entityManager);
 		
 		Conta conta = new Conta();
 		conta.setTitular("José Roberto");
 		conta.setBanco("Banco do Brasil");
 		conta.setNumero("123456-6");
-		conta.setAgencia("0999");
+		conta.setAgencia("0999"); 
 		
 		entityManager.getTransaction().begin();
-		entityManager.persist(conta);
+		dao.adiciona(conta);
 		entityManager.getTransaction().commit();
 		entityManager.close();
+		
 		
 		long fim = System.currentTimeMillis();
 		System.out.println("Executado em: " + (fim - inicio) + " ms");
